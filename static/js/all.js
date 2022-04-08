8072cbe4eb23c8faca48210100fefde3cef77ce5
Zarino Zappia <mail@zarino.co.uk>
2022-04-08 08:37:39 +0100

    Email alerts

--- static/js/all.js
+++ static/js/all.js
@@ -4,4 +4,22 @@ import $ from '../vendor/jquery/js/jquery.esm.js'
 $(function(){
     console.log('jQuery ready')
     console.log('Underscore.now()', _.now())
+
+    $('[data-show-when-checked]').each(function(){
+        var $el = $(this)
+        var $checkbox = $('#' + $el.attr('data-show-when-checked'))
+        var name = $checkbox.attr('name')
+
+        var updateUI = function(){
+            $el.toggleClass('d-none', ! $checkbox.is(':checked') )
+        }
+
+        $checkbox.on('change', updateUI)
+
+        if ( name && $('input[name="' + name + '"]').length ) {
+            $('input[name="' + name + '"]').on('change', updateUI)
+        }
+
+        updateUI()
+    })
 })
